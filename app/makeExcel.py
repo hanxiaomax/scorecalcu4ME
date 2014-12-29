7c99aab69179967ea8b268e94a6338f70c1fd946
lingfeng <hanxiaomax@qq.com>
2014-12-29 10:52:20 +0800

    1.按照Dreamweaver后，所有css和js失效，重新替换 2.基本完成excel模块

--- app/makeExcel.py
+++ app/makeExcel.py
@@ -1,10 +1,51 @@
+#coding:utf-8
 import xlwt
-from app import db,appME
+from app import db,appME,__StaticDir__
 from models import User, Score_items,ROLE_USER, ROLE_ADMIN,STATUS_YES , STATUS_NO , STATUS_UNKNOWN
 
-class querySomeOne():
 
-    def getUser(self,campID):
-        return user=get_user(campID)
-    def someOneDetail(campID)
-        getUser(campID)
+class MakeExcel(object):
+    """docstring for MakeExcel"""
+    def __init__(self):
+        self.workbook = xlwt.Workbook()
+        self.sheet = self.workbook.add_sheet(u'公示信息')
+        _tableTitle=[u"学号",u"姓名",u"年级",u"总分"]
+        for i in range(len(_tableTitle)):#Make table title
+            self.sheet.write(0,i,_tableTitle[i])
+        #TODO:make title
+
+    def _writerow(self,rowNo,infobuf):
+        """
+        @para:rowNo(write in row nomber x)
+        @para:infobuf(a dict contains someone's brief info)
+        """
+        _info=[infobuf["name"],infobuf["campID"],infobuf["grade"],infobuf["sum"]]
+        for i in range(len(_info)):
+            self.sheet.write(rowNo,i,_info[i])
+
+
+    def _getinfobuf(self,campID):
+        userInfoDict=User.userInfo(campID,is_jsonify=False,brief=True)
+        print userInfoDict
+        return userInfoDict#return as unicode,so do not need decode in write() function
+
+
+    def saveAs(self,filename):
+        self.workbook.save("F:/GIThub/scorecalcu4ME/app/static/1.xls")
+
+
+# userInfoDict={
+#              "name" : "name",
+#             "campID" : "campID",
+#             "grade" : "grade",
+#             "sum" : "score",
+#             }
+
+
+
+maker=MakeExcel()
+maker._writerow(1,maker._getinfobuf("130280"))
+maker.saveAs("F:/GIThub/scorecalcu4ME/app/static/1.xls")
+print __StaticDir__
+
+
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from flask.ext.login import (
 
 from models import User,Score_items, ROLE_USER, ROLE_ADMIN
 from login import LoginForm
-from app import appME, db, lm,getmyscore,saveapply,getreview
+from app import appME, db, lm,getmyscore,saveapply,getreview#,__StaticDir__
 from werkzeug import secure_filename,SharedDataMiddleware
 import os
 import json
@@ -20,6 +20,7 @@ def load_user(user_id):
 @appME.route('/login/',methods=['GET','POST'])
 #make this view function accepts GET and POST requests
 def login():
+    #print __StaticDir__
     form = LoginForm()
     if form.validate_on_submit():
         user=User.login_check(request.form.get('username'),request.form.get('password'))
@@ -137,7 +138,7 @@ def _getMyScore():
     elif(opt==0):
         return getmyscore._deleteapply()
     else:
-        return getmyscore._getTotal()
+        return getmyscore._getTotal()#Update the total score (called when searching or login)
 
 
 

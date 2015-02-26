914b880edfd0fafde6ce93e2f31d5b9415ff45cd
git config --global user.name hanxiaomaxgit config --global user.name <hanxiaomax@qq.com>
2015-02-26 23:07:41 +0800

    教师审核页面flexgrid排序和分页

---
+++ app/flexgrid.py
@@ -0,0 +1,48 @@
+#coding:utf-8
+from models import User, Score_items
+"""
+相应flexgrid排序和分页的函数
+"""
+
+
+def mycmp(a,b):
+    "自定义比较函数"
+    if a>b:
+        return 1
+    else:
+        return -1
+
+def sorter(before_sort,col,method):
+    """排序函数
+    args:
+        before_sort:传入的待排序列表
+        col：待排序的列
+        method：asc or desc
+    """
+    after_sort=sorted(before_sort,cmp=mycmp,key=lambda s:s[col],reverse=True if method=="asc" else False)
+    return after_sort
+
+
+def pageslicer(page,rp,_score_items):
+    """分页函数
+    args:
+        page:当前页数
+        rp：每页结果数
+        _score_items：全部条目列表
+    """
+    total=len(_score_items)
+    jsondict={
+        "page": page,#当前页
+        "total": total,#总数
+        "rows": []
+        }
+    for i in xrange((page-1)*rp,(page*rp if page*rp-1<total else total)):
+        s=_score_items[i]
+        #控件要求每行必须是如下结构的字典，cell中包含实际的数据
+        data={
+                "id": s.item_name,
+                "cell": User.getItemInfo(s)
+            }
+        jsondict["rows"].append(data)
+
+    return jsondict
